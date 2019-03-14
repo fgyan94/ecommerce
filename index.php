d635bb5a538ad08c43cb3be43bdb164aefcdee3e
Yan Gonçalves <fgyan94@gmail.com>
2019-03-14 20:52:48 -0300

    Admin - Categorias

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ require_once ("vendor/autoload.php");
 use Hcode\Page;
 use Hcode\PageAdmin;
 use Hcode\Model\User;
+use Hcode\Model\Category;
 use Slim\Slim;
 
 $app = new Slim ();
@@ -179,6 +180,78 @@ $app->post('/admin/forgot/reset', function(){
 	
 });
 
-$app->run ();
+$app->get('/admin/categories', function() {
+	User::verifyLogin();
+	
+	$categories = Category::listAll();
+	
+	$page = new PageAdmin();
+	$page->setTPL('categories', array(
+			'categories' => $categories
+	));
+});
+
+$app->get('/admin/categories/create', function() {
+	User::verifyLogin();
+	
+	$page = new PageAdmin();
+	$page->setTPL('categories-create');
+});
+	
+$app->post('/admin/categories/create', function() {
+	User::verifyLogin();
+	
+	$category = new Category();
+	
+	$category->setData($_POST);
+	
+	$category->save();
+	
+	header('Location: /admin/categories');
+	exit;
+});
+
+$app->get('/admin/categories/:idcategory/delete', function($idcategory) {
+	User::verifyLogin();
+	
+	$category = new Category();
+	
+	$category->get((int) $idcategory);
+	
+	$category->delete();
+	
+	header('Location: /admin/categories');
+	exit;	
+});
+
+$app->get('/admin/categories/:idcategory',function($idcategory) {
+	User::verifyLogin();
+	
+	$category = new Category();
+	
+	$category->get((int) $idcategory);
+	
+	$page = new PageAdmin();
+	$page->setTPL('categories-update', array(
+			'category' => $category->getValues()
+	));
+});
+	
+$app->post('/admin/categories/:idcategory',function($idcategory) {
+	User::verifyLogin();
+	
+	$category = new Category();
+	
+	$category->get((int) $idcategory);
+	
+	$category->setData($_POST);
+	
+	$category->save();
+	
+	header('Location: /admin/categories');
+	exit;	
+});
+
+$app->run();
 
 ?>
