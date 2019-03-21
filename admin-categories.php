19621b2c17e6c57b584f0712574ce135419f53bd
Yan Gonçalves <fgyan94@gmail.com>
2019-03-20 21:02:31 -0300

    Categorias vs Produtos

--- admin-categories.php
+++ admin-categories.php
@@ -4,6 +4,7 @@ use Hcode\PageAdmin;
 use Hcode\Page;
 use Hcode\Model\User;
 use Hcode\Model\Category;
+use Hcode\Model\Product;
 
 $app->get('/admin/categories', function() {
 	User::verifyLogin();
@@ -77,18 +78,54 @@ $app->post('/admin/categories/:idcategory',function($idcategory) {
 	exit;
 });
 	
-	$app->get('/categories/:idcategory', function($idcategory) {
-		
-		$category = new Category();
-		
-		$category->get((int) $idcategory);
-		
-		$page = new Page();
-		
-		$page->setTPL('category', array(
-				'category' => $category->getValues(),
-				'products' => array()
-		));
-	});
+$app->get('/admin/categories/:idcategory/products', function($idcategory) {
+	User::verifyLogin();
+	
+	$category = new Category();
+	
+	$category->get((int) $idcategory);
+	
+	$page = new PageAdmin();
+	
+	$page->setTPL('categories-products', array(
+			'category' => $category->getValues(),
+			'productsRelated' => $category->getProducts(),
+			'productsNotRelated' => $category->getProducts(false)
+	));
+});
+	
+$app->get('/admin/categories/:idcategory/products/:idproduct/add', function($idcategory, $idproduct) {
+	User::verifyLogin();
+	
+	$category = new Category();
+	
+	$category->get((int) $idcategory);
+	
+	$product = new Product();
+	
+	$product->get((int) $idproduct);
+	
+	$category->addProduct($product);
+	
+	header("Location: /admin/categories/$idcategory/products");
+	exit;
+});
+	
+$app->get('/admin/categories/:idcategory/products/:idproduct/remove', function($idcategory, $idproduct) {
+	User::verifyLogin();
+	
+	$category = new Category();
+	
+	$category->get((int) $idcategory);
+	
+	$product = new Product();
+	
+	$product->get((int) $idproduct);
+	
+	$category->removeProduct($product);
+	
+	header("Location: /admin/categories/$idcategory/products");
+	exit;
+});
 
 ?>
--- site.php
+++ site.php
@@ -2,6 +2,8 @@
 
 use \Hcode\Page;
 use Hcode\Model\Product;
+use Hcode\Model\User;
+use Hcode\Model\Category;
 
 $app->get ( '/', function () {
 	
@@ -13,5 +15,19 @@ $app->get ( '/', function () {
 			"products" => Product::checkList($products)
 	) );
 } );
+	
+$app->get('/categories/:idcategory', function($idcategory) {
+	
+	$category = new Category();
+	
+	$category->get((int) $idcategory);
+	
+	$page = new Page();
+	
+	$page->setTPL('category', array(
+			'category' => $category->getValues(),
+			'products' => Product::checkList($category->getProducts())
+	));
+});
 
 ?>
