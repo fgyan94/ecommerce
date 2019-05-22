2349ca9cd4a559bdf976a7e711293a1792161a7f
Yan Gonçalves <fgyan94@gmail.com>
2019-05-22 20:30:53 -0300

    Admin - Products Search and Pagination

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -157,7 +157,7 @@ class Category extends Model {
 								  ORDER BY descategory
 								  LIMIT $start, $itemsPerPage",
 				[
-						":search" => $search
+						":search" => "%$search%"
 				]);
 		
 		$nrTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal");
--- vendor/hcodebr/php-classes/src/Model/Product.php
+++ vendor/hcodebr/php-classes/src/Model/Product.php
@@ -141,5 +141,45 @@ class Product extends Model {
 				array(":idproduct" => $this->getidproduct())
 		);
 	}
+	
+	public static function getPage($page = 1, $itemsPerPage = 10) {
+		$start = ($page - 1) * $itemsPerPage;
+		
+		$sql = new Sql();
+		
+		$results = $sql->select("SELECT SQL_CALC_FOUND_ROWS *
+								FROM tb_products 
+								ORDER BY desproduct
+								LIMIT $start, $itemsPerPage");
+		
+		$nrTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal");
+		
+		return array("data" => $results,
+				"total" => (int) $nrTotal[0]['nrtotal'],
+				"pages" => ceil($nrTotal[0]['nrtotal'] / $itemsPerPage)
+		);
+	}
+	
+	public static function getPageSearch($search, $page = 1, $itemsPerPage = 10) {
+		$start = ($page - 1) * $itemsPerPage;
+		
+		$sql = new Sql();
+		
+		$results = $sql->select("SELECT SQL_CALC_FOUND_ROWS *
+								  FROM tb_products
+								  WHERE desproduct LIKE :search
+								  ORDER BY desproduct
+								  LIMIT $start, $itemsPerPage",
+				[
+						":search" => "%$search%"
+				]);
+		
+		$nrTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal");
+		
+		return array("data" => $results,
+				"total" => (int) $nrTotal[0]['nrtotal'],
+				"pages" => ceil($nrTotal[0]['nrtotal'] / $itemsPerPage)
+		);
+	}
 }
 ?>
